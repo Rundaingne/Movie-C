4d70061b5f5e0900d7b00654aec99c9bc781e0b9
Erich Kumpunen <erichtheviolin@gmail.com>
2019-03-29 09:07:19 -0600

    Initial Commit

---
+++ MovieSearch-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieSearch-C
+//
+//  Created by Brooke Kumpunen on 3/29/19.
+//  Copyright © 2019 Rund LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
